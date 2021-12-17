fff54855e07cb9eea738da19c3bbc1107d10b2db
Irene Veselova <ik1vikuz@gmail.com>
2021-12-17 23:14:59 +0300

    try to add functions

--- src/main.js
+++ src/main.js
@@ -62,6 +62,33 @@ const userData = [
   },
 ]
 
+const data = {
+  names: [],
+  descriptions: [],
+  portraits: [],
+  id: 7
+}
+
+const addUser = function() {
+  let nameRandom = data.names[Math.floor(Math.random() * data.names.length)];
+  let descriptionRandom = data.descriptions[Math.floor(Math.random() * data.descriptions.length)];
+  let portraitRandom = data.portraits[Math.floor(Math.random() * data.portraits.length)];
+  let idRandom = Math.floor((Math.random() * 1000) + data.id);
+
+  userData.push({id: idRandom, name: nameRandom, description: descriptionRandom, portrait: portraitRandom});
+}
+
+const delUser = function(e) {
+  console.log(e.target.value)
+  // console.log(userData.find((el) => el.id === e.target.id))
+}
+
+const getData = userData.map((user) => {
+  data.names.push(user.name);
+  data.descriptions.push(user.description);
+  data.portraits.push(user.portrait);
+});
+
 const vnode = h("div", { class: { container: true } },
   [h("div", { class: { users: true } },
     [h("ul", { class: { users__list: true } },
@@ -74,16 +101,16 @@ const vnode = h("div", { class: { container: true } },
             h("h2", { class: { user__name: true } }, user.name),
             h("p", { class: { user__description: true } }, user.description)],
           ),
-          h("button", { class: { 'user__delete-btn': true, 'delete-btn': true } }, [
+          h("button", { class: { 'user__delete-btn': true, 'delete-btn': true }, on: { click: delUser } }, [
             h("span"),
             h("span")
           ])
         ])
       )
     ),
-    h("button", { class: { 'users__add-btn': true, 'add-btn': true } }, '+')
+    h("button", { class: { 'users__add-btn': true, 'add-btn': true }, on: { click: addUser } }, '+')
     ])
   ]
 );
 
-patch(app, vnode);
+patch(app, vnode, getData);
